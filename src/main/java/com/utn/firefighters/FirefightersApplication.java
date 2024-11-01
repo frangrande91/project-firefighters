3e85b76b1b129523017daa5e5e8ee33a0ebcdbeb
FeDeJMendez <fedemendezg@gmail.com>
2024-11-01 20:56:20 -0300

    Init

---
+++ src/main/java/com/utn/firefighters/FirefightersApplication.java
@@ -0,0 +1,13 @@
+package com.utn.firefighters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirefightersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirefightersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/firefighters/FirefightersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.firefighters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirefightersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
