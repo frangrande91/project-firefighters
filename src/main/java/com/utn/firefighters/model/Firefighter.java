c15aea0b5a140320649a300b4c1ef7dedc294401
franGrande <franciscogrande91@gmail.com>
2024-11-04 22:52:37 -0300

    Add models

---
+++ src/main/java/com/utn/firefighters/model/Firefighter.java
@@ -0,0 +1,16 @@
+package com.utn.firefighters.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "firefighters")
+public class Firefighter extends Person {
+
+}
